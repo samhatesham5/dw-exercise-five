aa9b3ce62c83d8cfadba1d3a1f383d9df07089ce
Sam Whitley <samwhitley@10-16-33-71.dynapool.wireless.nyu.edu>
2022-11-08 12:28:25 -0500

    nov 8th class

--- routes/createPost.js
+++ routes/createPost.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+//Creating the form for this post
+const createPostForm = `
+<h1> Create Post </h1> 
+<form action = "">
+    <input type="text" name ="title" placeholder="Title" />
+    <input type="text" name ="title" placeholder="Title" />
+    <input type="text" name ="title" placeholder="Title" />
+    <button type="submit">Submit</button>
+</form>`; 
+
 //Middleware that is specific to this router
 //_ = if you're not using a parameter in js, you can use this
-router.use((_,_,next) => {
+router.use((req,res,next) => {
     next();
 });
 
 router.get("/", (req,res) => {
-    res.send("hi");
+    res.send(createPostForm);
 });
 
 
-modeule.exports = router; 
+module.exports = router; 
